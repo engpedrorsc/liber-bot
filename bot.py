af80e1dec072f78eac01cbfdba3b2eb0e8fe0e89
engpedrorsc <60117628+engpedrorsc@users.noreply.github.com>
2020-12-13 01:18:39 -0300

    Minor

--- bot.py
+++ bot.py
@@ -18,6 +18,7 @@ Criar executável: pyinstaller --clean -F bot.py
 FUNCTIONS
 '''
 
+
 def open_browser(driver):
     profile = webdriver.FirefoxProfile()
     profile.set_preference('intl.accept_languages', 'en')
@@ -43,7 +44,7 @@ def get_msg(driver, url, wdw):
 
 
 def send_msg(consumer_key, consumer_secret, key, secret, msg):
-    #Twitter authentication
+    # Twitter authentication
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(key, secret)
 
@@ -66,7 +67,6 @@ def countdown(n, step, text_plural, text_singular):
             print("Contagem regressiva com número negativo.")
             exit()
 
-        
         n -= step
         sleep(step)
         print(" " * max(len(count_plural), len(count_singular)), end="\r")
@@ -76,14 +76,13 @@ def countdown(n, step, text_plural, text_singular):
 MAIN
 '''
 
+
 def main():
     driver = open_browser('geckodriver.exe')
-    wdw = WebDriverWait(driver, 15, poll_frequency=0.5, ignored_exceptions=None)
+    wdw = WebDriverWait(driver, 15, poll_frequency=0.5,
+                        ignored_exceptions=None)
     url = 'http://www.quantocustaobrasil.com.br/2012/widget_300x220_txt/'
-    consumer_key = '123zKkQFrW6ia1cWQWzHalYzy'
-    consumer_secret = 'ZTH9mg82M6KdPFG4XwK1a7IbAzzwCAg9ZF5x8qQSfUkW2MPq4v'
-    key = '1312168239724625920-glxwhYBiMeh2cgpvuKwGgaCUtv3ziW'
-    secret = 'qVlIcbqAMeMyR3agQFnISTru5swQjCNH7gRevPXdLHnew'
+    # inserir chaves
 
     evasion_post_hours = [7, 12, 20]
     for hour in evasion_post_hours:
@@ -96,13 +95,14 @@ def main():
         if evasion_post_hours[evasion_index] == dt.now().hour:
             text = get_msg(driver, url, wdw)
             send_msg(consumer_key, consumer_secret, key, secret, text)
-            evasion_index +=1
+            evasion_index += 1
             if evasion_index >= len(evasion_post_hours):
                 evasion_index = 0
         else:
             print(f'Tentativa de envio em {dt.now()}')
-        
-        countdown(20*60, 10, 'Nova tentativa em {} segundos.', 'Nova tentativa em {} segundo.')
+
+        countdown(20*60, 10, 'Nova tentativa em {} segundos.',
+                  'Nova tentativa em {} segundo.')
 
 
 if __name__ == '__main__':
